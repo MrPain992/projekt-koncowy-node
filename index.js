39f8a1b92ea2b0325bb25c146eafe40e5aaf2b7a
4ia1 <4ia1>
2016-11-30 11:19:02 +0100

    listen port fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var port = 80;
+var port = process.env.PORT || 80;
 
 app.use(express.static('public'));
 
